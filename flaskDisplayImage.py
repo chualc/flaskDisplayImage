cdf8d3073a6f48b954a33932b5618bce25a76670
chualc <32863283+chualc@users.noreply.github.com>
2018-11-22 20:45:35 +0800

    Added Programming is so easy

--- flaskDisplayImage.py
+++ flaskDisplayImage.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('home.html')
+    imageList = ['Programming.JPG', 'isSo.JPG', 'Easy.JPG']
+    return render_template('home.html', imageList=imageList )
 
 
 if __name__ == '__main__':
